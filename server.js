b400116ef6f1187a002f10d74374185f7fd19df5
John Wells <molekewl2017@gmail.com>
2019-11-05 00:45:41 -0800

    get request with knex

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const knex = require('knex')
 
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.get('/', (req, res) => {
+  db
+    .select('*')
+    .from('accounts')
+    .then(account => res.status(200).json(account))
+    .catch(err => res.status(500).json({ error: 'Failed to retrieve accounts'}))
+})
 
 module.exports = server;
