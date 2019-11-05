e8b7f81d093145c66244bb7745efd1f1e3be6122
John Wells <molekewl2017@gmail.com>
2019-11-05 01:14:13 -0800

    add & update records

--- server.js
+++ server.js
@@ -13,6 +13,29 @@ server.get('/', (req, res) => {
     .from('accounts')
     .then(account => res.status(200).json(account))
     .catch(err => res.status(500).json({ error: 'Failed to retrieve accounts'}))
+});
+
+// Add new record to db
+server.post('/', (req, res) => {
+  knex
+    .insert(req.body, 'id')
+    into('accounts')
+    .then(record => res.status(200).json(record))
+    .catch(err => console.log().json({ error: 'Failed to add record to database' }));
 })
+
+
+// update record to db
+server.put('/:id', (req, res) => {
+  const params = req.body;
+
+  db('accounts')
+    .where({ id: req.params.id })
+    .update(params)
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(err => console.console.log().json({ error: 'Failed to update account record' }));
+});
 
 module.exports = server;
